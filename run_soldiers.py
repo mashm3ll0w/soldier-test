6cdc9a3b6f505352f3b836f0eaa5cbe54698943b
Charles Swaleh <charlswall@live.com>
2019-10-02 20:45:03 +0300

    define the main functions that a user will interact with

--- run_soldiers.py
+++ run_soldiers.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python3
+
+from soldiers import Soldier
+
+def create_soldier(svc_num, rank, name, unit, corps):
+  new_soldier = Soldier(svc_num, rank, name, unit, corps)
+
+  return new_soldier
+
+def save_soldiers(soldier):
+  Soldier.save_soldier()
+
+def list_soldiers():
+  return Soldier.display_soldiers()
+
+def remove_soldier(soldier):
+  return Soldier.delete_soldier()
+
+def find_soldier(number):
+  return Soldier.find_by_svcNumber(number)
+
+def check_soldier_exists(number):
+  return Soldier.check_if_exists(number)
+
+def copy_soldier_svcNumber(number):
+  return Soldier.copy_svcNumber(number)
+
