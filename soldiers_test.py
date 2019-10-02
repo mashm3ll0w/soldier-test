cbc745ad9d59d9e94fbcd4632bcc0d178f89a1a3
Charles Swaleh <charlswall@live.com>
2019-10-02 16:22:31 +0300

    test the objects are instantiated correctly

--- soldiers_test.py
+++ soldiers_test.py
@@ -9,5 +9,12 @@ class TestSoldier(unittest.TestCase):
   def tearDown(self):
     Soldier.soldier_list = []
 
+  def test_init(self):
+    self.assertEqual(self.new_soldier.svc_num, "102030")
+    self.assertEqual(self.new_soldier.rank, "Private")
+    self.assertEqual(self.new_soldier.name, "Karl")
+    self.assertEqual(self.new_soldier.unit, "3 Rifles")
+    self.assertEqual(self.new_soldier.corps, "Infantry")
+
 if __name__ == '__main__':
   unittest.main()
